440211f933a7f1d046feb606057dd423d90ae37f
wwilco <wgmwilcox@gmail.com>
2015-03-16 18:38:32 -0400

    port is on 3000 for the time being

---
+++ server.js
@@ -0,0 +1,27 @@
+var WebSocketServer = require("ws").Server;
+var server = new WebSocketServer({port : 3000});
+
+var clients = [];
+
+server.on("connection", function(ws) {
+
+  clients.push(ws);
+
+  ws.on("close", function() {
+    var x = clients.indexOf(ws);
+    clients.splice(x,1);
+    console.log("Clients connected: " + clients.length);
+  });
+
+  ws.on("message", function(input) {
+
+    processedInput = JSON.parse(input);
+    console.log(processedInput.name + " : " + processedInput.text);
+
+    for (i = 0; i < clients.length; i++) {
+      clients[i].send(input);
+    }
+
+  });
+});
+console.log("listening on port 3000!")
